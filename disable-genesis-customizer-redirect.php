5d941f7b4f9ed5753d735c71668071dd3f3bd86d
jonschr <jonschr@gmail.com>
2019-05-27 17:43:15 -0500

    Fix for php notice in strict mode (create default object from empty value)

--- disable-genesis-customizer-redirect.php
+++ disable-genesis-customizer-redirect.php
@@ -5,7 +5,7 @@
  * Description: Access the theme settings page without redirecting to customizer
  * Author:      Bill Erickson
  * Author URI:  https://www.billerickson.net
- * Version:     1.0.0
+ * Version:     1.0.1
  */
 
 /**
@@ -14,7 +14,7 @@
  */
 function be_disable_genesis_redirect() {
 	global $_genesis_admin_settings, $_genesis_admin_seo_settings;
-	$_genesis_admin_settings->redirect_to = false;
-	$_genesis_admin_seo_settings->redirect_to = false;
+	$_genesis_admin_settings->redirect_to = '/wp-admin/admin.php?page=genesis&noredirect';
+	$_genesis_admin_seo_settings->redirect_to = '/wp-admin/admin.php?page=seo-settings&noredirect';
 }
 add_action( 'genesis_admin_menu', 'be_disable_genesis_redirect', 11 );
